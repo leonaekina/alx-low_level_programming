5e6844573d3b389ee4e2468f9c33785c128cf55c
leonaekina <leonaekina@gmail.com>
2023-02-21 13:57:08 -0800

    Q1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
