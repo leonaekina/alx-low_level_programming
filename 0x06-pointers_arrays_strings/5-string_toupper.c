3101d49245926e9bb4237565c128022dde102b47
leonaekina <leonaekina@gmail.com>
2023-03-01 14:31:08 -0800

    Q5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,33 +1,20 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes everey word of a string
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
  * @s: string to modify
  *
  * Return: the resulting string
  */
-char *cap_string(char *s)
+char *string_toupper(char *s)
 {
-	int i, j;
-
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
-		'!', '?', '"', '(', ')', '{', '}'};
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-
-		for (j = 0; j < 13; j++)
-		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
-			}
-		}
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 	}
 
 	return (s);
