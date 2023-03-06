3161128f9085274e5185e9cd0a6e059767bf4049
leonaekina <leonaekina@gmail.com>
2023-03-06 14:30:01 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+/*
+ * prototype functions
+*/
+
+int _putchar(char c);
+
+char *_memset(char *s, char b, unsigned int n);
+
+void set_string(char **s, char *to);
+
+void print_diagsums(int *a, int size);
+
+void print_chessboard(char (*a)[8]);
+
+char *_strstr(char *haystack, char *needle);
+
+char *_strpbrk(char *s, char *accept);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strchr(char *s, char c);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
