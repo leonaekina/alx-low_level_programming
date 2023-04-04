95f123e3de2511b574bb746a933844ae6c54b3b0
leonaekina <leonaekina@gmail.com>
2023-04-04 05:56:38 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -17,9 +17,9 @@ size_t print_listint(const listint_t *h)
 
 	while (current_node != NULL)
 	{
-		printf("%d/d", current_node->n);
-		node_count++;
+		printf("%d/n", current_node->n);
 		current_node = current_node->next;
+		node_count++;
 	}
 
 	return (node_count);
