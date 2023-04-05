11766e5a2e5a423edbe99d428e7382d57ffc472b
leonaekina <leonaekina@gmail.com>
2023-04-04 17:07:15 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,17 +10,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current_node;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
 	while (head != NULL)
 	{
-		current_node = *head;
+		temp = *head;
 		*head = (*head)->next;
-		free(current_node);
+		free(temp);
 	}
-
-	*head = NULL;
 }
