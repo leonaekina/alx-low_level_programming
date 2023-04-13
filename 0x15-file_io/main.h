a6ae1968fbc447f2167a781d0c0d2d3f34fe7838
leonaekina <leonaekina@gmail.com>
2023-04-13 15:14:15 -0700

    Main Header

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,22 @@
+#ifndef FILES_H
+#define FILES_H
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
