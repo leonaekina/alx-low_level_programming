a9942160f1f2103fae316b11857da396f77a8779
leonaekina <leonaekina@gmail.com>
2023-04-13 04:26:05 -0700

    Task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks endianess
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *j = (char *)&i;
+
+	if (*j)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
