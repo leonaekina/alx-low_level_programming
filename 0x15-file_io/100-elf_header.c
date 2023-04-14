e76d83f041ca69c27ffba265cf53e65d3388dbf3
leonaekina <leonaekina@gmail.com>
2023-04-14 10:00:45 -0700

    Task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -17,10 +17,10 @@ void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
-
 /**
- * check_elf - Checks if a file is an ELF file
+ * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
  * Description: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
@@ -241,6 +241,8 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 
 	else
 		printf("%#lx\n", e_entry);
+}
+
 /**
  * close_elf - Closes an ELF file.
  * @elf: The file descriptor of the ELF file.
